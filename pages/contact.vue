b46172ef58eb5e5b4dddd5a7a2e58ff9e052a85a
Fabien Udriot <fabien@omic.ch>
2019-09-30 18:05:14 +0200

    Add margin top to display the Map

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,12 +1,19 @@
 <template>
-  <div class="row">
-    <div class="col-sm">
-      <div><strong>Edouard Compaoré</strong></div>
-      <div>Téléphone: +226 75 25 00 52</div>
-      <div>Téléphone: +226 78 92 49 57</div>
+  <div>
+
+    <div class="row">
+      <div class="col-sm">
+        <div><strong>Edouard Compaoré</strong></div>
+        <div>Téléphone: +226 75 25 00 52</div>
+        <div>Téléphone: +226 78 92 49 57</div>
+      </div>
+      <div class="col-sm">
+        <img src="/edouard-compaore.jpeg" alt="" width="350">
+      </div>
     </div>
-    <div class="col-sm">
-      <img src="/edouard-compaore.jpeg" alt="" width="350">
+
+    <div id="map" style="margin-top: 30px">
+      [TODO: carte de ouagadougou avec un marker qui pointe sur la ville de Ouaga]
     </div>
   </div>
 </template>
